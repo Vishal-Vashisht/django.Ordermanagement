fc74e8c89316c57ec11cf4c095240b584d92b761
---list <vashisth671@gmail.com>
2025-12-28 18:16:55 +0530

    cors allow origin changes

--- src/ordermanagement/ordermanagement/settings/settings.py
+++ src/ordermanagement/ordermanagement/settings/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 INSTALLED_APPS += LOCAL_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -58,6 +59,14 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+
+def env_list(key, default=""):
+    return [x.strip() for x in os.getenv(key, default).split(",") if x.strip()]
+
+
+CORS_ALLOWED_ORIGINS = env_list('CORS_ALLOWED_ORIGINS')
+CORS_ALLOW_CREDENTIALS = True
 
 AUTH_USER_MODEL = "accounts.User"
 ROOT_URLCONF = "ordermanagement.urls"
